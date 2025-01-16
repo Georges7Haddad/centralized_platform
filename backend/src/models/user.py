038953bb906f6b3d6cb18f56e9deeb129dd548ca
Ahmad Dimashkie <afd08@mail.aub.edu>
2025-01-16 23:19:07 +0200

    Created the connection to the database from the backend, along with a test that tests the connection
    git push origin fixed_database_setup

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -4,4 +4,10 @@ import strawberry
 @strawberry.type
 class User:
 	first_name: str
+<<<<<<< HEAD
 	age: int
+=======
+	last_name: str
+	username: str
+	password: str
+>>>>>>> 2071a19 (Created the connection to the database from the backend, along with a test that tests the connection)
