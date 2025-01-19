03d403516617c4347fac61374db4c2a28e3da3ff
Ahmad Dimashkie <afd08@mail.aub.edu>
2025-01-19 20:12:38 +0200

    missed one file while fixing the github conflict

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -4,10 +4,4 @@ import strawberry
 @strawberry.type
 class User:
 	first_name: str
-<<<<<<< HEAD
 	age: int
-=======
-	last_name: str
-	username: str
-	password: str
->>>>>>> 2071a19 (Created the connection to the database from the backend, along with a test that tests the connection)
