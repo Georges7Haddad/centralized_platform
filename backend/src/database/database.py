21e5f864ff1b62cfacf35ba306b336c172866243
Ahmad Dimashkie <afd08@mail.aub.edu>
2025-01-23 12:41:09 +0200

    Changed the database connection from root user to IAM user but it is still not complete this is just a template that needs to be fixed

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -1,25 +1,52 @@
 import os
 
+import boto3
 from dotenv import load_dotenv
+from sqlalchemy.engine.url import URL
 from sqlmodel import Session, create_engine
 
-# Load the database_config.env file that is in the same directory as this file
 load_dotenv(
 	dotenv_path=os.path.join(os.path.dirname(__file__), "database_config.env")
 )
+pem_file_path = os.path.join(os.path.dirname(__file__), "eu-west-3-bundle.pem")
 
 DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
 DATABASE_HOST = os.getenv("DATABASE_HOST")
 DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+AWS_REGION = os.getenv("AWS_REGION")
 
 
-DATABASE_URL = f"mysql+pymysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
-print("DATABASE_URL:", DATABASE_URL)
+def generate_iam_auth_token():
+	client = boto3.client("rds", region_name=AWS_REGION)
+	token = client.generate_db_auth_token(
+		DBHostname=DATABASE_HOST,
+		Port=int(DATABASE_PORT),
+		DBUsername=DATABASE_USERNAME,
+	)
+	return token
+
+
+DATABASE_PASSWORD = generate_iam_auth_token()
+
+DATABASE_URL = str(
+	URL.create(
+		drivername="mysql+pymysql",
+		username=DATABASE_USERNAME,
+		password=DATABASE_PASSWORD,
+		host=DATABASE_HOST,
+		port=DATABASE_PORT,
+		database=DATABASE_NAME,
+	)
+)
+
 # A SQLModel engine is what holds the connections to the database.
 # You would have one single engine object for all your code to connect
 # to the same database.
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+	DATABASE_URL,
+	connect_args={"ssl": {"ssl_ca": pem_file_path}},
+)
 
 
 # A Session is what stores the objects in memory and keeps track of any changes
