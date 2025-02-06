452740dd024fd46daa3e31141c73742f0f92cd81
Ahmad Dimashkie <afd08@mail.aub.edu>
2025-02-07 00:19:03 +0200

    created the base of the course page still needs work though

---
+++ frontend/src/app/courses/[course]/sections.js
@@ -0,0 +1,9 @@
+export default function Sections() {
+  return (
+    <div className="mt-6">
+      this is the sections of the course I am thinking here of having a list of
+      all the sections given for this course (with the different crns, and
+      timings and instructors)
+    </div>
+  );
+}
