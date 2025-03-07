aa3101ddb54e54ae65395f87d90a169e2687b6c7
Mariam Elwirish <mariamelwirish@gmail.com>
2025-03-08 01:35:27 +0200

    removed 'read all students', followed documentation for session, modified 'read_student_courses' to return objects instead of crns

--- backend/src/endpoints/student_endpoints.py
+++ backend/src/endpoints/student_endpoints.py
@@ -1,6 +1,11 @@
-from fastapi import HTTPException
+from fastapi import Depends, HTTPException
 from sqlmodel import Session, select
-from src.database.database import get_session_dependency
+
+# from src.database.database import get_session
+from src.endpoints.local_connection import get_session
+
+# from src.server import app
+from src.endpoints.local_session import app
 from src.models.course_model import Course
 from src.models.student_model import (
 	Student,
@@ -9,7 +14,8 @@ from src.models.student_model import (
 	StudentCreate,
 	StudentUpdate,
 )
-from src.server import app
+
+get_session_dependency = Depends(get_session)
 
 
 # create a student
@@ -17,30 +23,20 @@ from src.server import app
 def create_student(
 	student: StudentCreate, session: Session = get_session_dependency
 ):
-	with session:
-		db_student = Student.model_validate(student)
-		session.add(db_student)
-		session.commit()
-		session.refresh(db_student)
-		return db_student
-
-
-# read all students data
-@app.get("/students/", response_model=list[StudentBase])
-def read_students(session: Session = get_session_dependency):
-	with session:
-		students = session.exec(select(Student)).all()
-		return students
+	db_student = Student.model_validate(student)
+	session.add(db_student)
+	session.commit()
+	session.refresh(db_student)
+	return db_student
 
 
 # read one student
 @app.get("/students/{student_id}", response_model=StudentBase)
 def read_student(student_id: int, session: Session = get_session_dependency):
-	with session:
-		student = session.get(Student, student_id)
-		if not student:
-			raise HTTPException(status_code=404, detail="Student not found")
-		return student
+	student = session.get(Student, student_id)
+	if not student:
+		raise HTTPException(status_code=404, detail="Student not found")
+	return student
 
 
 # register student in course
@@ -48,17 +44,16 @@ def read_student(student_id: int, session: Session = get_session_dependency):
 def register_student_to_course(
 	student_id: int, course_crn: int, session: Session = get_session_dependency
 ):
-	with session:
-		student = session.get(Student, student_id)
-		course = session.get(Course, course_crn)
-		if not student:
-			raise HTTPException(status_code=404, detail="Student not found")
-		if not course:
-			raise HTTPException(status_code=404, detail="Course not found")
-		link = StudentCourseLink(student_id=student_id, course_crn=course_crn)
-		session.add(link)
-		session.commit()
-		return {"message": "Student added to course"}
+	student = session.get(Student, student_id)
+	course = session.get(Course, course_crn)
+	if not student:
+		raise HTTPException(status_code=404, detail="Student not found")
+	if not course:
+		raise HTTPException(status_code=404, detail="Course not found")
+	link = StudentCourseLink(student_id=student_id, course_crn=course_crn)
+	session.add(link)
+	session.commit()
+	return {"message": "Student added to course"}
 
 
 # get all courses for a student
@@ -66,16 +61,20 @@ def register_student_to_course(
 def read_student_courses(
 	student_id: int, session: Session = get_session_dependency
 ):
-	with session:
-		statement = select(StudentCourseLink).where(
-			StudentCourseLink.student_id == student_id
+	statement = (
+		select(Course)
+		.join(
+			StudentCourseLink,
+			StudentCourseLink.course_crn == Course.crn,
 		)
-		course_crns = session.exec(statement).all()
-		if not course_crns:
-			raise HTTPException(
-				status_code=404, detail="No courses found for this student."
-			)
-		return course_crns
+		.where(StudentCourseLink.student_id == student_id)
+	)
+	courses = session.exec(statement).all()
+	if not courses:
+		raise HTTPException(
+			status_code=404, detail="No courses found for this student."
+		)
+	return courses
 
 
 # update a student
@@ -85,31 +84,27 @@ def update_student(
 	student: StudentUpdate,
 	session: Session = get_session_dependency,
 ):
-	with session:
-		db_student = session.get(Student, student_id)
-		if not db_student:
-			raise HTTPException(status_code=404, detail="Student not found")
-		student_data = student.model_dump(exclude_unset=True)
-		db_student.sqlmodel_update(student_data)
-		session.add(db_student)
-		session.commit()
-		session.refresh(db_student)
-		return db_student
+	db_student = session.get(Student, student_id)
+	if not db_student:
+		raise HTTPException(status_code=404, detail="Student not found")
+	student_data = student.model_dump(exclude_unset=True)
+	db_student.sqlmodel_update(student_data)
+	session.add(db_student)
+	session.commit()
+	session.refresh(db_student)
+	return db_student
 
 
 # delete a student
 @app.delete("/students/{student_id}")
 def delete_student(student_id: int, session: Session = get_session_dependency):
-	with session:
-		student = session.get(Student, student_id)
-		if not student:
-			raise HTTPException(status_code=404, detail="Student not found")
+	student = session.get(Student, student_id)
+	if not student:
+		raise HTTPException(status_code=404, detail="Student not found")
 
-		session.query(StudentCourseLink).filter_by(
-			student_id=student_id
-		).delete()
+	session.query(StudentCourseLink).filter_by(student_id=student_id).delete()
 
-		session.delete(student)
-		session.commit()
+	session.delete(student)
+	session.commit()
 
-		return {"ok": True}
+	return {"ok": True}
