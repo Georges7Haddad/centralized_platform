78c252a07bd3e64e6d4600c7fc46b64fec9318b6
tonyabihaidar <tga22@mail.aub.edu>
2025-03-24 09:39:42 +0200

    fixed ruff check errors

--- backend/src/models/messageChatService_model.py
+++ backend/src/models/messageChatService_model.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 from sqlmodel import Field, SQLModel
 
+
 class MessageBase(SQLModel):
     sender: Annotated[str, Field(min_length=3, max_length=100)]
     receiver: Annotated[str, Field(min_length=3, max_length=100)]
