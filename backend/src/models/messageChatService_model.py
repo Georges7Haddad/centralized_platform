4abca817be72214a29df727752593c4a9f7ee90a
tonyabihaidar <tga22@mail.aub.edu>
2025-03-26 08:56:45 +0200

    moved message id

--- backend/src/models/messageChatService_model.py
+++ backend/src/models/messageChatService_model.py
@@ -6,13 +6,14 @@ from sqlmodel import Field, SQLModel
 class MessageBase(SQLModel):
     sender: Annotated[str, Field(min_length=3, max_length=100)]
     receiver: Annotated[str, Field(min_length=3, max_length=100)]
+    message_id: int = Field(primary_key=True)
     content: str
     timestamp: str
     read: bool
     deleted: bool
 
 class Message(MessageBase, table=True):
-    message_id: int = Field(primary_key=True)
+    pass
 
 class MessageCreate(MessageBase):
     pass
