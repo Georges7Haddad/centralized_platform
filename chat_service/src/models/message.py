d09942ba16ae2900ca89a189ff26e354b444a6bf
tonyabihaidar <tga22@mail.aub.edu>
2025-04-02 21:03:58 +0300

    message model

---
+++ chat_service/src/models/message.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+
+from sqlmodel import Field, SQLModel
+
+
+class MessageBase(SQLModel):
+    sender: Annotated[str, Field(min_length=3, max_length=100)]
+    receiver: Annotated[str, Field(min_length=3, max_length=100)]
+    message_id: int = Field(primary_key=True)
+    content: str
+    timestamp: str
+    read: bool
+    deleted: bool
+
+class Message(MessageBase, table=True):
+    pass
+
+class MessageCreate(MessageBase):
+    pass
+
+
