3e8e866d3330ba1eb5be31d09ba71bef252c1a80
Mariam Elwirish <mariamelwirish@gmail.com>
2025-04-04 06:32:10 +0300

    created chat model

---
+++ chat_service/src/models/chat.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from datetime import datetime
+from sqlmodel import SQLModel, Field, Relationship
+
+class Chat(SQLModel, table=True):
+    user_id: int = Field(foreign_key="user.id", primary_key=True)
+    chat_id: str = Field(primary_key=True) 
+    last_message_id: int = None
+    last_updated: datetime = Field(default_factory=datetime.utcnow)
+
+    # Relationships
+    user: Optional["UserBase"] = Relationship(back_populates="chats")
