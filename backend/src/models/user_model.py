5432d76718cb2411fa901bf021b664da465c558d
Mariam Elwirish <mariamelwirish@gmail.com>
2025-04-10 00:32:48 +0300

    testing

--- backend/src/models/user_model.py
+++ backend/src/models/user_model.py
@@ -1,5 +1,5 @@
-#from chat_service.src.models.chat import Chat
-#from chat_service.src.models.group import Group, GroupMember
+# from chat_service.src.models.chat import Chat
+# from chat_service.src.models.group import Group, GroupMember
 from sqlmodel import Field, SQLModel
 
 
@@ -12,11 +12,11 @@ class UserBase(SQLModel):
 	major: str
 	email: str
 
-    # Relationships (strings to avoid circular import issues)
-	#created_groups: list["Group"] = Relationship(back_populates="created_by")
-	#group_memberships: list["GroupMember"] = Relationship(back_populates="user")
-	#chats: list["Chat"] = Relationship(back_populates="user_chats")
-	
+	# Relationships (strings to avoid circular import issues)
+	# created_groups: list["Group"] = Relationship(back_populates="created_by")
+	# group_memberships: list["GroupMember"] = Relationship(back_populates="user")
+	# chats: list["Chat"] = Relationship(back_populates="user_chats")
+
 
 # User create model (for data creation)
 class UserCreate(UserBase):
